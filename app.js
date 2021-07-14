9fa3217c97ac2858b91c5d5a344a6ab224ad294a
Akash Ratan Verma <vermaakashratan232@gmail.com>
2021-07-14 14:21:39 +0530

    Add MongoDB database and small changes in frontend

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ =require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const homeStartingContent = " This is a normal personal blog website where you can compose a post with title and its content. All the posts that are composed by you will be shown on the home page and as you keep on adding new post they will directly be shownn on the home page. ";
+const aboutContent = "I'm learning web development so this website is made by the frameworks and technologies which I have learned so far. the database used for this website is MongoDB.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let posts = [];
 
 const app = express();
 
@@ -17,12 +17,27 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postSchema = {
+  title: String,
+  postDetail: String
+}
+
+const Post = mongoose.model("Post",postSchema);
+
+
 app.get("/",function(req,res){
 
-  res.render("home", { 
-    homeText : homeStartingContent,
-    Posts: posts
+  Post.find({}, function(err,posts){
+    if(!err){
+      res.render("home", { 
+        homeText : homeStartingContent,
+        posts: posts
+      });
+    }
   });
+  
 });
 
 app.get("/about",function(req,res){
@@ -30,7 +45,7 @@ app.get("/about",function(req,res){
 });
 
 app.get("/contact",function(req,res){
-  res.render("contact", { contactText : contactContent});
+  res.render("contact", {});
 });
 
 app.get("/compose",function(req,res){
@@ -38,26 +53,31 @@ app.get("/compose",function(req,res){
 });
 
 app.post("/compose",function(req,res){
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     postDetail: req.body.postBody
-  };
+  });
 
-  posts.push(post);
-  res.redirect("/");
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
+  
 });
 
-app.get("/post/:postName",function(req,res){
+app.get("/post/:postId",function(req,res){
 
-  var pn = _.lowerCase(req.params.postName);
+  const postID = req.params.postId;
 
-  posts.forEach(function(post){
+  Post.findOne({_id: postID}, function(err,post){
 
-    if(pn === _.lowerCase(post.title) )
-      res.render("post",{
-        Title: post.title,
-        Detail: post.postDetail
-      });
+    if(!err){
+        res.render("post",{
+          Title: post.title,
+          Detail: post.postDetail
+        });
+    }
 
   });
 
